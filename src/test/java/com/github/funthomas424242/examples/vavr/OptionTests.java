a42f8bb19567e6087bce6a94c424f369509c7631
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-01-11 22:49:48 +0100

    Option Tests added

---
+++ src/test/java/com/github/funthomas424242/examples/vavr/OptionTests.java
@@ -0,0 +1,48 @@
+package com.github.funthomas424242.examples.vavr;
+
+import io.vavr.control.Option;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OptionTests {
+
+
+
+    @Test
+    @DisplayName("Option handle NPE with NoneDefault")
+    public void option_toHandleNPE_withNoneDefault() {
+
+        final Option<String> messageText = Option.of(null);
+
+        assertEquals("None", messageText.toString());
+    }
+
+
+    @Test
+    @DisplayName("Option handle NPE with UserdefinedDefault")
+    public void option_toHandleNPE_withUserDefinedDefault() {
+
+        final Option<String> messageText = Option.of(null);
+
+        assertEquals("DefaultValue", messageText.getOrElse("DefaultValue"));
+    }
+
+    @Test
+    @DisplayName("Option handle NPE with NoSuchElementException")
+    public void option_toHandleNPE_withNoSucheElementException() {
+
+        final Option<String> messageText = Option.of(null);
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> {
+            messageText.get();
+        });
+    }
+
+
+
+}
