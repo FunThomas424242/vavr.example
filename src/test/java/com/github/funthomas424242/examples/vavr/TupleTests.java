6e1e7f6db366122ee0588499a688a4cc38880367
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-01-12 15:53:13 +0100

    Tupel Tests added

---
+++ src/test/java/com/github/funthomas424242/examples/vavr/TupleTests.java
@@ -0,0 +1,29 @@
+package com.github.funthomas424242.examples.vavr;
+
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import io.vavr.Tuple3;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class TupleTests {
+
+    @Test
+    public void useValidBiTupel() {
+        final Tuple2<String, Integer> tupel = Tuple.of("Java", 8);
+        Assertions.assertEquals("Java", tupel._1);
+        Assertions.assertEquals((Integer) 8, tupel._2());
+    }
+
+    @Test
+    public void useValidTriTupel() {
+        final Tuple3<String, String, LocalDate> tupel = Tuple.of("Thomas", "Schubert", LocalDate.of(1968, 12, 25));
+
+        Assertions.assertEquals("Thomas",tupel._1);
+        Assertions.assertEquals("Schubert",tupel._2);
+        Assertions.assertEquals(LocalDate.of(1968,12,25),tupel._3);
+    }
+
+}
